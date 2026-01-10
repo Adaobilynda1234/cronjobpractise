d04a12ef24ab893ae51677db8b23b022af0db542
Adaobi Okwuosa <adaobilynda1234@gmail.com>
2026-01-10 22:05:08 +0100

    code update

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const scheduleRoutes = require("./routes/schedule.routes");
 const app = express();
 app.use(express.json());
 
+// Routes
+
 app.use("/api/schedules", scheduleRoutes);
 
 module.exports = app;
